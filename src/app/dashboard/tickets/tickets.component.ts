1d259ec5ee1d8aa3640fce46eeccbc0b9ce870cf
diamondback7ee <138242154+diamondback7ee@users.noreply.github.com>
2025-01-25 02:18:45 +0000

    version changes

---
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+
+@Component({
+  selector: "app-tickets",
+  templateUrl: "./tickets.component.html",
+  styleUrl: "./tickets.component.css",
+  imports: [NewTicketComponent],
+})
+export class TicketsComponent {}
---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,15 @@
+import { Component, input, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-control",
+  imports: [],
+  templateUrl: "./control.component.html",
+  styleUrl: "./control.component.css",
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: "control",
+  },
+})
+export class ControlComponent {
+  label = input.required<string>();
+}
